a3eaa37ed56d82f348eafde0234f3c54d0a4187a
Feng <yao.feng@accenture.com>
2020-12-03 18:14:49 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      122222For a guide and recipes on how to configure / customize this project,<br>
+      12222123123122For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
