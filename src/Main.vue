9605f9ead03f8d6095216d952ffcb6b47519107b
Feng <yao.feng@accenture.com>
2020-12-07 18:43:49 +0800

    1

---
+++ src/Main.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div id="app">
+    <h1>Hello App!</h1>
+    <p>
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+      <router-link class="link" to="/foo">Go to Foo</router-link>
+      <router-link class="link" to="/bar">Go to Bar</router-link>
+    </p>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
+
+<style>
+.link { display: block; }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router';
+import App from './Main.vue'
+import Foo from './components/Foo.vue';
+import Bar from './components/Bar.vue';
+
+Vue.use(VueRouter)
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
